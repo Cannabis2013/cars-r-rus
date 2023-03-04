9525bcdcfbc16f3013dbbe33db086a5091b9e4dc
Cannabis2013 <MH2012@outlook.dk>
2023-03-04 16:08:49 +0100

    Updated something

--- src/main/java/dat3/car/config/dbInit/cars/Nissan/NissanShitbox.java
+++ src/main/java/dat3/car/config/dbInit/cars/Nissan/NissanShitbox.java
@@ -6,26 +6,34 @@ import dat3.car.entities.cars.Car;
 public class NissanShitbox {
     public NissanShitbox() {
         _car = new Car();
-        _car.setBrand("Nissan");
-        _car.setModel("Shitbox");
-        _car.setColor("white");
-        _car.setYear(1983);
+        updateBasicDetails();
         _car.setDescription("""
                 Allright, this is not one of the popular choices. With nearly everything broken
                 this car is not for the faint of hearth. But it comes with a low price.
                 """);
+        updateFeatures();
+        _car.setImageFilePath("/home/mh/GitRepos/cars_backend/src/main/java/dat3/car/config/dbInit/cars/Nissan/nissan_shitbox_1983.png");
+    }
+
+    public Car getCar() {
+        return _car;
+    }
+
+    private void updateBasicDetails() {
+        _car.setBrand("Nissan");
+        _car.setModel("Shitbox");
+        _car.setColor("white");
+        _car.setYear(1983);
+        _car.setPricePrDay(300);
+    }
+
+    private void updateFeatures(){
         _car.getFeatures().put("Missing tires",
                 "One tire is missing. Please provide one yourself");
         _car.getFeatures().put("Chernobog Transparency",
                 "It comes with full A/C with all windows broken, but with no fuel consumption");
         _car.getFeatures().put("Chernobogs Engine Dismantle","Engine is dismantled and is located in the trunk");
         _car.getFeatures().put("Chernobog Raw Metal Look","Nothing but bare metal interior design");
-        _car.setPricePrDay(300);
-        _car.setImageFilePath("/home/mh/GitRepos/cars_backend/src/main/java/dat3/car/config/dbInit/cars/Nissan/nissan_shitbox_1983.png");
-    }
-
-    public Car getCar() {
-        return _car;
     }
 
     private Car _car;
--- src/main/java/dat3/car/config/dbInit/cars/VW/VWID4.java
+++ src/main/java/dat3/car/config/dbInit/cars/VW/VWID4.java
@@ -1,29 +1,44 @@
 package dat3.car.config.dbInit.cars.VW;
 
-import dat3.car.contracts.Utils.IImageConverter;
 import dat3.car.entities.cars.Car;
 
 public class VWID4{
     public VWID4() {
         _car = new Car();
-        _car.setBrand("Volkswagen");
-        _car.setModel("ID.4");
-        _car.setYear(2021);
-        _car.setColor("black");
+        updateBasicDetails();
+        updateFeatures();
+        _car.setImageFilePath("/home/mh/GitRepos/cars_backend/src/main/java/dat3/car/config/dbInit/cars/VW/vw_id4.jpg");
+        _car.setPricePrDay(4500);
         _car.setDescription("""
                 Volkswagens popular electric vehicle.
                 """);
-        _car.getFeatures().put("Chernobog Transparency",
-                "A broken front window and missing left rear window");
-        _car.getFeatures().put("Road Condition Feature",
-                "To massive holes in the floor located at the back section");
-        _car.setPricePrDay(4500);
-        _car.setImageFilePath("/home/mh/GitRepos/cars_backend/src/main/java/dat3/car/config/dbInit/cars/VW/vw_id4.jpg");
 
     }
 
     public Car getCar() {
         return _car;
+    }
+
+    private void updateBasicDetails(){
+        _car.setBrand("Volkswagen");
+        _car.setModel("ID.4");
+        _car.setYear(2021);
+        _car.setColor("black");
+    }
+
+    private void updateFeatures(){
+        _car.getFeatures().put("Chernobog Transparency",
+                """
+                        A broken front window and missing left rear window provides
+                        excellent A/C at no fuel consumption cost. Great at summer season,
+                        not so much at winter.
+                        """);
+        _car.getFeatures().put("Road Condition Feature",
+                """
+                        Two massive holes in the floor located at the back section
+                        enables a great opportunity to asses the road quality from inside
+                        the car while driving.
+                        """);
     }
 
     private Car _car;
