b6882f4d5e3e22354faf9d157248fc7b2d9f66e3
Cannabis2013 <MH2012@outlook.dk>
2023-03-18 01:08:26 +0100

    Removed dateformatter annotations

--- src/main/java/dat3/car/reservations/dtos/ReservationRequest.java
+++ src/main/java/dat3/car/reservations/dtos/ReservationRequest.java
@@ -14,8 +14,6 @@ import java.time.LocalDateTime;
 public final class ReservationRequest {
     private String carId;
     private String memberName;
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime reservationStart;
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime reservationEnd;
 }
